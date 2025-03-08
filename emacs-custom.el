34c22d3645a5fda8632b53cb8f3225c0ff49a6db
calx <108835591+null-calx@users.noreply.github.com>
2025-03-08 22:35:45 +0530

    add custom-file

---
+++ emacs-custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-vivendi))
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     default))
+ '(mouse-wheel-flip-direction t)
+ '(mouse-wheel-tilt-scroll t)
+ '(package-selected-packages
+   '(auctex cdlatex csv-mode dockerfile-mode elcord gruber-darker-theme
+	    haskell-mode htmlize jump-char magit multiple-cursors
+	    nginx-mode popup-kill-ring sage-shell-mode slime sly
+	    svelte-mode terraform-mode treemacs typescript-mode
+	    which-key yaml-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "ADBO" :family "Source Code Pro")))))
--- init.el
+++ init.el
@@ -6,21 +6,7 @@
 
 (package-initialize)
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(setq custom-file "~/.emacs.d/emacs-custom.el")
+(load custom-file)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(modus-vivendi))
- '(mouse-wheel-flip-direction t)
- '(mouse-wheel-tilt-scroll t)
- '(package-selected-packages
-   '(treemacs sly svelte-mode terraform-mode csv-mode yaml-mode jump-char multiple-cursors htmlize sage-shell-mode nginx-mode dockerfile-mode elcord auctex cdlatex magit typescript-mode haskell-mode slime which-key)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "ADBO" :family "Source Code Pro")))))
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
